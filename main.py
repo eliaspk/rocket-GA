51e18ef4ac015089af26afbf268117dc3e5a2fd9
eliaspk <eliask1094@gmail.com>
2024-06-04 15:54:03 -0300

    Inrease screen size

--- main.py
+++ main.py
@@ -1,8 +1,8 @@
 from environment import Environment
 
 if __name__ == "__main__":
-  width = 800
-  height = 600
+  width = 1280
+  height = 720
   pop_size = 50
 
   env = Environment(width, height, 50)
